837065fee2238fecfacd06b0217f48aa60d2eb8d
sungone <20200999@sungshin.ac.kr>
2023-04-19 21:31:24 +0900

    #30 feat : 회원가입/로그인 POST RES 토큰 추가

--- src/main/java/com/zatch/zatchserver/controller/UserController.java
+++ src/main/java/com/zatch/zatchserver/controller/UserController.java
@@ -70,7 +70,7 @@ public class UserController {
             HttpHeaders header = new HttpHeaders();
             header.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-            GetUserResDto getUserResDto = new GetUserResDto(Long.valueOf(userId), postUserReqDto.getName(), nickname, postUserReqDto.getEmail());
+            GetUserResDto getUserResDto = new GetUserResDto(Long.valueOf(userId), postUserReqDto.getName(), nickname, postUserReqDto.getEmail(), accessToken);
 
             return ResponseEntity.ok().body(getUserResDto);
         }
@@ -83,7 +83,7 @@ public class UserController {
         String refreshToken = String.valueOf(authService.issueRefreshToken(Long.valueOf(userId)));
         response.addHeader("ACCESS_TOKEN", refreshToken);
 
-        GetUserResDto getUserResDto = new GetUserResDto(Long.valueOf(userId), postUserReqDto.getName(), nickname, email);
+        GetUserResDto getUserResDto = new GetUserResDto(Long.valueOf(userId), postUserReqDto.getName(), nickname, email, refreshToken);
 
         return ResponseEntity.ok().body(getUserResDto);
     }
