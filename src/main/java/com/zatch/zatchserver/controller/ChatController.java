9ef2ec55b11b97625cceabf8263574cca81dffec
sungone <20200999@sungshin.ac.kr>
2023-05-11 20:15:43 +0900

    #46 feat : 응답모델적용, 별점후기등록 수정

--- src/main/java/com/zatch/zatchserver/controller/ChatController.java
+++ src/main/java/com/zatch/zatchserver/controller/ChatController.java
@@ -4,11 +4,13 @@ import com.zatch.zatchserver.DefaultRes;
 import com.zatch.zatchserver.ResponseMessage;
 import com.zatch.zatchserver.StatusCode;
 import com.zatch.zatchserver.domain.ChatRoom;
+import com.zatch.zatchserver.dto.GetBlockResDto;
 import com.zatch.zatchserver.dto.PostChatReqDto;
 import com.zatch.zatchserver.service.ChatService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -51,14 +53,20 @@ public class ChatController {
     }
 
     //(거래 후) 별점, 후기 등록하기
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = PostChatReqDto.class,
+                    examples = @Example(@ExampleProperty(value = "{'property1': 'value1', 'property2': 'value2'}", mediaType = MediaType.APPLICATION_JSON_VALUE)))
+    })
     @PostMapping("/after_deal")
     @ApiOperation(value = "별점&후기 등록", notes = "별점&후기 등록 API")
-    public ResponseEntity postStarReview(HttpServletRequest req) {
+    public ResponseEntity postStarReview(@RequestBody PostChatReqDto postChatReqDto) {
         try {
-            Long send_user_id = Long.valueOf(req.getParameter("send_user_id"));
-            Long receive_user_id = Long.valueOf(req.getParameter("receive_user_id"));
-            String review_context = req.getParameter("review_context");
-            int star_rating = Integer.parseInt(req.getParameter("star_rating"));
+            Long send_user_id = postChatReqDto.getSend_user_id();
+            Long receive_user_id = postChatReqDto.getReceive_user_id();
+            String review_context = postChatReqDto.getReview_context();
+            int star_rating = postChatReqDto.getStar_rating();
+            System.out.println("send_user_id : " + send_user_id);
             chatService.after_deal(send_user_id, receive_user_id, review_context, star_rating);
             return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.AFTER_CHAT_SUCCESS, new PostChatReqDto(send_user_id, receive_user_id, review_context, star_rating)), HttpStatus.OK);
         } catch (Exception e) {
