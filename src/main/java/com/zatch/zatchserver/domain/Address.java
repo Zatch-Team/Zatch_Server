45897dee5fdc7923b8da57f2dbd25d393ed0fa89
plum-king <jeesoo0908@gmail.com>
2023-07-14 23:52:43 +0900

    feat: 거리 계산 로직 추가 #19

--- src/main/java/com/zatch/zatchserver/domain/Address.java
+++ src/main/java/com/zatch/zatchserver/domain/Address.java
@@ -6,13 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Address {
+    private Long user_id;
     private String addr_name;
     private String addr_x;
     private String addr_y;
 
     public Address(){};
 
-    public Address(String addr_name, String addr_x, String addr_y) {
+    public Address(Long user_id, String addr_name, String addr_x, String addr_y) {
+        this.user_id = user_id;
         this.addr_name = addr_name;
         this.addr_x = addr_x;
         this.addr_y = addr_y;
--- src/main/java/com/zatch/zatchserver/repository/MainRepositoryImpl.java
+++ src/main/java/com/zatch/zatchserver/repository/MainRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,7 +22,7 @@ public class MainRepositoryImpl implements MainRepository {
     }
 
     @Override
-    public String getMainTown(Long userId){
+    public String getMainTown(Long userId) {
         String sql = "SELECT main_addr_name FROM user WHERE user_id = ?;";
         Object[] params = {userId};
         String main_addr_name = String.valueOf(jdbcTemplate.queryForList(sql, params).get(0).get("main_addr_name"));
@@ -34,13 +35,16 @@ public class MainRepositoryImpl implements MainRepository {
         Object[] params = {userId};
         Double main_addr_x = Double.valueOf(String.valueOf(jdbcTemplate.queryForList(sql, params).get(0).get("main_addr_x")));
         Double main_addr_y = Double.valueOf(String.valueOf(jdbcTemplate.queryForList(sql, params).get(0).get("main_addr_y")));
+        Double dist;
+        List<Long> dist_suc = new ArrayList<>();
 
         List<Address> addresses = jdbcTemplate.query(
-                "SELECT main_addr_name, main_addr_x, main_addr_y FROM user WHERE user_id in (SELECT user_id FROM zatch);",
+                "SELECT user_id, main_addr_name, main_addr_x, main_addr_y FROM user WHERE user_id in (SELECT user_id FROM zatch);",
                 new RowMapper<Address>() {
                     @Override
                     public Address mapRow(ResultSet rs, int rowNum) throws SQLException {
                         Address address = new Address(
+                                rs.getLong("user_id"),
                                 rs.getString("main_addr_name"),
                                 String.valueOf(rs.getDouble("main_addr_x")),
                                 String.valueOf(rs.getDouble("main_addr_y"))
@@ -48,6 +52,21 @@ public class MainRepositoryImpl implements MainRepository {
                         return address;
                     }
                 });
+
+        for (int i = 0; i < addresses.size(); i++) {
+            System.out.println(addresses.get(0));
+            Double theta = Double.parseDouble(addresses.get(i).getAddr_y()) - main_addr_y;
+            dist = Math.sin(Double.parseDouble(addresses.get(i).getAddr_x()) * Math.PI / 180.0) * Math.sin(main_addr_x * Math.PI / 180.0) + Math.cos(Double.parseDouble(addresses.get(i).getAddr_y()) * Math.PI / 180.0) * Math.cos(main_addr_y * Math.PI / 180.0) * Math.cos(theta * Math.PI / 180.0);
+
+            dist = Math.acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515 * 1.609344;
+            if(dist <= 1.0){
+                dist_suc.add(addresses.get(i).getUser_id());
+            }
+        }
+        System.out.println(dist_suc);
+
 
         List<ViewNearZatch> results = jdbcTemplate.query(
                 "SELECT zatch.user_id, zatch_id, category_id, is_free, md_name, content, zatch.updated_at, quantity, date_buy, date_expire, is_opened, any_zatch, like_count, main_addr_name FROM zatch JOIN user on zatch.user_id = user.user_id;",
@@ -75,7 +94,7 @@ public class MainRepositoryImpl implements MainRepository {
     }
 
     @Override
-    public List<ViewPopularZatch> getPopularZatch(Long userId){
+    public List<ViewPopularZatch> getPopularZatch(Long userId) {
         List<ViewPopularZatch> results = jdbcTemplate.query(
                 "SELECT user_id, zatch_id, category_id, is_free, md_name, content, updated_at, quantity, date_buy, date_expire, is_opened, any_zatch, like_count FROM zatch ORDER BY like_count DESC LIMIT 3;",
                 new RowMapper<ViewPopularZatch>() {
